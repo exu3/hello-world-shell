72f4700f2d79e8dc83bc0479c92b7c9b006c3d4c
eilla1 <72365100+eilla1@users.noreply.github.com>
2021-01-28 21:37:39 -0800

    :earth_americas: hello :rocket:

---
+++ hello.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# This is a comment lol!
+echo Hello World	# this is another comment hihihihi
---
+++ moreHello.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# tis a comment
+echo "Hello 	World"	# tis anoter comment
+echo "Hello World"
+echo "Hello * World"
+echo Hello World
+echo Hello 	world
+echo "hello" world
+echo Hello "	" world
+echo "Hello "*" World"
+echo hello world
+echo 'hello' world
